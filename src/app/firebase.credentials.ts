7a4aa93d2c5bfd310abf0cf8302a908e30b368cf
wellingtonlins <wellingtonlins2013@gmail.com>
2018-07-10 17:54:11 -0300

    Just add item in the firebase

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAW-iPNc0I8zu-guQtMmfdbY0IPXigfndM",
+    authDomain: "teste-firebase-14596.firebaseapp.com",
+    databaseURL: "https://teste-firebase-14596.firebaseio.com",
+    projectId: "teste-firebase-14596",
+    storageBucket: "teste-firebase-14596.appspot.com",
+    messagingSenderId: "994221144479"
+}
